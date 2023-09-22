840a61f0a3dd8adef3558b02159c1e45209e9a16
aamrutabet <aamrutabet@gmail.com>
2023-09-22 15:21:14 +0530

    Login form changes

--- frontend/code_busters_ui/src/app/login/login.component.ts
+++ frontend/code_busters_ui/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { ApiService } from '../services/api-services'
+import { LoginData } from '../modules/LoginData';
+import { LoginResponse } from '../modules/LoginResponse';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +13,34 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 
 export class LoginComponent {
  
-  loginForm = this.fb.group({
+  loginForm = this.login.group({
     userName: ['',Validators.required],
     secret: ['', Validators.required]
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private apiService : ApiService, private login: FormBuilder, private _router : Router) { }
   
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.log(this.loginForm.value);
+
+    const loginData = new FormData();
+    let password = this.loginForm.value.secret;
+    let username = this.loginForm.value.userName;
+    loginData.append('username', username??'');
+    loginData.append('password',  password??'');
+    let lData = {
+      'username': username,
+      'password': password
+    }
+    this.apiService.navigatorLogin(lData).subscribe((data: any) => {
+      console.log('Response'+data.id+data.first_name+data.last_name+data.role);
+      //this.navigateTo('/dashboard');
+    },(error) => {
+      console.log('Error'+error);
+    })
+  }
+  navigateTo(page:String) {
+    this._router.navigate([page]);
   }
 }
--- frontend/code_busters_ui/src/app/services/api-services.ts
+++ frontend/code_busters_ui/src/app/services/api-services.ts
@@ -1,23 +1,37 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { LoginResponse } from "../modules/LoginResponse";
 
 @Injectable({
     providedIn: "root"
 })
 
 export class ApiService {
-    private baseURl = '';
+    private baseURl = 'https://mibg93bqyi.us-east-1.awsapprunner.com';
 
     constructor(private httpClient: HttpClient) {
     }
 
-    public navigatorLogin(): Observable<any> {
-        return this.httpClient.get(this.baseURl + '/login', {
+     public navigatorLogin (loginData : any): Observable<any> {
+        console.log('Request'+loginData);
+        return this.httpClient.post('https://mibg93bqyi.us-east-1.awsapprunner.com/login', loginData, {
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*'
-            }
+            },
+            responseType : 'json'
         });
     }
+
+    // public navigatorLogin (loginData :FormData): Observable<LoginResponse> {
+    //     console.log('Request'+loginData);
+    //     return this.httpClient.get<LoginResponse>(this.baseURl + '/login', {
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Access-Control-Allow-Origin': '*'
+    //         },
+    //         responseType : 'json'
+    //     });
+    // }
 }

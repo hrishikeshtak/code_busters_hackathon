f33bd21a111fadde894e8bcfc73d29ab5559a640
aamrutabet <aamrutabet@gmail.com>
2023-09-22 15:23:06 +0530

    Login form changes

---
+++ frontend/code_busters_ui/src/app/modules/LoginData.ts
@@ -0,0 +1,9 @@
+export class LoginData {
+    userName : String;
+    password : String;
+
+    constructor(userName : String, password:String){
+        this.userName = userName;
+        this.password = password;
+    }
+}

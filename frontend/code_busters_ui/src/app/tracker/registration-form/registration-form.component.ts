397e1cbeb9fc24557afceac834b826f8371fdcc2
Nikita Ramtri <nikitaramtri@gmail.com>
2023-09-22 18:41:24 +0530

    Temporary commit

--- frontend/code_busters_ui/src/app/tracker/registration-form/registration-form.component.ts
+++ frontend/code_busters_ui/src/app/tracker/registration-form/registration-form.component.ts
@@ -7,16 +7,20 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent {
-  registrationForm: FormGroup;
+  registrationForm:FormGroup;
 
-  constructor(private formBuilder: FormBuilder){
-    this.registrationForm = formBuilder.group([
-      // firstName = ['', Validators.required],
-      // lastName = ['', Validators.required]
-    ]);
+  constructor(private fb: FormBuilder) { 
+    this.registrationForm = this.fb.group({
+      firstName: ['',Validators.required],
+      lastName: ['', Validators.required],
+      dob: [new Date(), Validators.required],
+      contactNumber: ['(000) 000 0000', Validators.required],
+      location: ['', Validators.required]
+    });
   }
-
-  onSubmit(){
+  
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
     console.log(this.registrationForm.value);
   }
 }

e6338471bdd3e738f5e1c6129c0a4a1123cfe732
Nikita Ramtri <nikitaramtri@gmail.com>
2023-09-22 22:57:04 +0530

    Added changes for registration page

--- frontend/code_busters_ui/src/app/tracker/tracker.component.ts
+++ frontend/code_busters_ui/src/app/tracker/tracker.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tracker',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class TrackerComponent {
 
+  constructor(private _router: Router){ }
+  
+  navigateTo(path: string){
+    this._router.navigate([path]);
+  }
 }
